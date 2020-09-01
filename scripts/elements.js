f97df2ef45223bc39ec4c6c519280e65dcc05e31
evinracher <eevinley@gmail.com>
2020-09-01 18:01:26 -0500

    Adding a class to about section

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,7 +1,6 @@
 // Selecting elements
 export const mobileBtn = document.querySelector('.mobile-button');
 export const mobileNav = document.querySelector('.mobile-nav');
-export const videoDisplayer = document.querySelector('.about__video-displayer');
 export const teamMembers = document.querySelectorAll('.team-member');
 export const portafolioImages = document.querySelectorAll('.portafolio__image');
 export const questions = document.querySelectorAll('.question');
